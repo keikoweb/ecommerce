386822b07bcedb4bea61d9910a10c77ab1d55ec6
keikoweb <keiko77mel@gmail.com>
2019-11-07 06:47:25 +1100

    add the cart function

--- src/shops/models.py
+++ src/shops/models.py
@@ -37,30 +37,30 @@ class UserProfile(models.Model):
 class Item(models.Model):
     title = models.CharField(max_length=100)
     price = models.FloatField()
-    # discount_price = models.FloatField(blank=True, null=True)
-    # category = models.CharField(choices=CATEGORY_CHOICES, max_length=2)
-    # label = models.CharField(choices=LABEL_CHOICES, max_length=1)
-    # slug = models.SlugField()
-    # description = models.TextField()
-    # image = models.ImageField()
+    discount_price = models.FloatField(blank=True, null=True)
+    category = models.CharField(choices=CATEGORY_CHOICES, max_length=2)
+    label = models.CharField(choices=LABEL_CHOICES, max_length=1)
+    slug = models.SlugField()
+    description = models.TextField()
+    image = models.ImageField()
 
     def __str__(self):
         return self.title
 
-    # def get_absolute_url(self):
-    #     return reverse("core:product", kwargs={
-    #         'slug': self.slug
-    #     })
+    def get_absolute_url(self):
+        return reverse("shops:product", kwargs={
+            'slug': self.slug
+        })
 
-    # def get_add_to_cart_url(self):
-    #     return reverse("core:add-to-cart", kwargs={
-    #         'slug': self.slug
-    #     })
+    def get_add_to_cart_url(self):
+        return reverse("shops:add-to-cart", kwargs={
+            'slug': self.slug
+        })
 
-    # def get_remove_from_cart_url(self):
-    #     return reverse("core:remove-from-cart", kwargs={
-    #         'slug': self.slug
-    #     })
+    def get_remove_from_cart_url(self):
+        return reverse("shops:remove-from-cart", kwargs={
+            'slug': self.slug
+        })
 
 
 class OrderItem(models.Model):
@@ -70,8 +70,8 @@ class OrderItem(models.Model):
     item = models.ForeignKey(Item, on_delete=models.CASCADE)
     quantity = models.IntegerField(default=1)
 
-    # def __str__(self):
-    #     return f"{self.quantity} of {self.item.title}"
+    def __str__(self):
+        return f"{self.quantity} of {self.item.title}"
 
     # def get_total_item_price(self):
     #     return self.quantity * self.item.price
--- src/shops/urls.py
+++ src/shops/urls.py
@@ -1,17 +1,18 @@
 from django.urls import path
 from .views import (
-    HomeView, OrderSummaryView, ItemDetailView
+    CategoryView, HomeView, OrderSummaryView, ItemDetailView, add_to_cart, remove_from_cart
 )
 app_name = 'shops'
 
 urlpatterns = [
+    path('category/', CategoryView.as_view(), name='category'),
     path('', HomeView.as_view(), name='home'),
     # path('checkout/', CheckoutView.as_view(), name='checkout'),
     path('order-summary/', OrderSummaryView.as_view(), name='order-summary'),
     path('product/<slug>/', ItemDetailView.as_view(), name='product'),
-    # path('add-to-cart/<slug>/', add_to_cart, name='add-to-cart'),
+    path('add-to-cart/<slug>/', add_to_cart, name='add-to-cart'),
     # path('add-coupon/', AddCouponView.as_view(), name='add-coupon'),
-    # path('remove-from-cart/<slug>/', remove_from_cart, name='remove-from-cart'),
+    path('remove-from-cart/<slug>/', remove_from_cart, name='remove-from-cart'),
     # path('remove-item-from-cart/<slug>/', remove_single_item_from_cart,
     #      name='remove-single-item-from-cart'),
     # path('payment/<payment_option>/', PaymentView.as_view(), name='payment'),
